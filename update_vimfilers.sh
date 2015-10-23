b0e0fbb8adc2d2e36e3084f81a6e0683a96f2f67
whlin <whlin626@gmail.com>
2015-10-24 00:45:09 +0800

    create update_vimfilers.sh

---
+++ update_vimfilers.sh
@@ -0,0 +1,6 @@
+cp -r ./vim $RCPATH/vimfilers/vim
+cp ./vimrc $RCPATH/vimfilers/vimrc
+cp ./gvimrc $RCPATH/vimfilers/gvimrc
+
+#cp ./zshrc ~/rc_files/zshrc
+#cp ./whlin.zsh-theme ~/rc_files/whlin.zsh-theme
