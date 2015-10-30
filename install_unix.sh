538069fcfbf1844dc41c41d325686f3ba9a2b829
whlin <whlin626@gmail.com>
2015-10-30 17:54:31 +0800

    install_unix.sh add bundle setting

--- install_unix.sh
+++ install_unix.sh
@@ -4,6 +4,7 @@
 rm ~/.vim    | ln -s `pwd` ~/.vim
 rm ~/.vimrc  | ln -s `pwd`/vimrc ~/.vimrc 
 rm ~/.gvimrc | ln -s `pwd`/gvimrc ~/.gvimrc 
+rm -rf ~/.bundle & mkdir ~/.bundle & ln -s ~/.bundle `pwd`/bundle
 
 ## Install Neobundle
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
