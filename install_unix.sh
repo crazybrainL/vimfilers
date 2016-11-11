e88b10d23b66b55c295ea6706926771e930c69a1
Wu-Hsuan Lin <wuhsuan_lin@trend.com.tw>
2016-11-11 15:52:50 +0800

    Update install_unix.sh

--- install_unix.sh
+++ install_unix.sh
@@ -28,15 +28,15 @@ fi
 
 ln -s ~/.bundle `pwd`/bundle
 
-### Install Neobundle
-#curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+## Install Neobundle
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 
-### Install all plugins
-#vim +NeoBundleInstall +qall
+## Install all plugins
+vim +NeoBundleInstall +qall
 
-### make vimproc
-#cd ~/.vim/bundle/vimproc.vim && make
+## make vimproc
+cd ~/.vim/bundle/vimproc.vim && make
 
-### install YouCompleteMe
-##cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive && ./install.sh --clang-completer --gocode-completer --tern-completer
-#cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive && ./install.py
+## install YouCompleteMe
+#cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive && ./install.sh --clang-completer --gocode-completer --tern-completer
+cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive && ./install.py
